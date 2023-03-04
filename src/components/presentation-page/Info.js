547136cb306ac65e986f997c83e7acfc31847682
andradaCojocaru <andrada.cojocaru@stud.acs.upb.ro>
2023-03-04 20:31:11 +0200

    first commit

---
+++ src/components/presentation-page/Info.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// reactstrap components
+import { Container, Row, Col } from "reactstrap";
+
+// Core Components
+
+function Info() {
+  return (
+    <>
+      <section className="section-info">
+        <Container>
+          <Row>
+            <Col lg="4" md="6">
+              <div className="info text-left">
+                <div className="icon icon-lg icon-shape icon-shape-primary shadow rounded-circle">
+                  <i className="ni ni-satisfied"></i>
+                </div>
+                <h6 className="info-title text-uppercase text-primary pl-0">
+                  Recomanda un medic
+                </h6>
+                <p className="description opacity-8">
+                  Spune tuturor despre experienta pe care ai avut-o
+                </p>
+                <Link className="text-primary" to="/index">
+                  Recomanda{" "}
+                  <i className="ni ni-bold-right text-primary"></i>
+                </Link>
+              </div>
+            </Col>
+            <Col lg="4" md="6">
+              <div className="info text-left" style={{ marginTop: "-50px" }}>
+                <div className="icon icon-lg icon-shape icon-shape-success shadow rounded-circle">
+                  <i className="ni ni-collection"></i>
+                </div>
+                <h6 className="info-title text-uppercase text-success pl-0">
+                  Afla povestile altora
+                </h6>
+                <p className="description opacity-8">
+                  Lasa-te inspirat de povestile oamenilor care au trecut prin experiente asemanatoare
+            
+                </p>
+                <Link className="text-primary" to="/sections">
+                  Citeste experientele altora{" "}
+                  <i className="ni ni-bold-right text-primary"></i>
+                </Link>
+              </div>
+            </Col>
+            <Col lg="4" md="6">
+              <div className="info text-left">
+                <div className="icon icon-lg icon-shape icon-shape-warning shadow rounded-circle">
+                  <i className="ni ni-single-copy-04"></i>
+                </div>
+                <h6 className="info-title text-uppercase text-warning pl-0">
+                  Povestea ta
+                </h6>
+                <p className="description opacity-8">
+                  Simte-te liber sa povesti prin ceea ce ai trecut
+                </p>
+                <a
+                  className="text-primary"
+                  href="#pablo"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  Scrieti povestea{" "}
+                  <i className="ni ni-bold-right text-primary"></i>
+                </a>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
+}
+
+export default Info;
